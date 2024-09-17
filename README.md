5810d849baafea8f2af65bb5eeb3a9c2fca0606d
Michella Stuhr Bech <144211387+MichellaBech@users.noreply.github.com>
2024-09-17 10:39:11 +0200

    setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Startcode for Java Deep Dive II week
+Includes:
+
+- pom.xml
+- HibernatConfig
+- .gitignore
---
+++ src/main/java/dat/Main.java
@@ -0,0 +1,11 @@
+package dat;
+
+import dat.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
+
+    }
+}
